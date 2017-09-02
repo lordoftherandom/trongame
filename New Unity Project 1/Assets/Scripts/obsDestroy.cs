2c228c175d88cb0e96054064fed279f8490e0f90
lordoftherandom <dietokillfun@gmail.com>
2017-09-02 16:05:43 -0400

    Update

---
+++ New Unity Project 1/Assets/Scripts/obsDestroy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obsDestroy : MonoBehaviour {
+	private float xRange; //Distance until object should be destroyed
+	private GameObject spawnerHolder;
+	// Use this for initialization
+	void Start () {
+		xRange = 15.0f;
+		spawnerHolder = GameObject.FindGameObjectWithTag ("Spawner");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x < -xRange) { //xRange is negative to make it easier to understand when editing
+			spawnerHolder.GetComponent<Spawner>().Remove(this.gameObject);
+			Destroy (this.gameObject);
+		}
+	}
+}
