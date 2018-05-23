239c4daf09fc57bcf58ae972edd95054ffa5064b
lordoftherandom <dietokillfun@gmail.com>
2018-05-22 21:43:28 -0400

    Enum addition, Spawning Spawners
    Added Enum script with extended methods
    
    Add spawning spawners to the map script

---
+++ New Unity Project 1/Assets/Scripts/MapHandling/Map.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour {
+    private GameObject spawner;
+    private List <GameObject> allSpawners;
+    private readonly string[] objs = { "Circle", "Cube", "Prisim" };
+    private int[] spwnWeights = { 5, 1, 1 }; //We want the most circles to spawn
+    private const float DEF_SPAWNTM = 60;
+    private float spwnrTm, maxSpwnrTm;
+    private int diff;
+    
+
+	// Use this for initialization
+	void Start () {
+        diff = 1;
+        allSpawners = new List<GameObject>();
+        spawner = Resources.Load("Spawner", typeof(GameObject)) as GameObject;
+        if(spawner == null)
+            Debug.Log("I guess progress");
+        maxSpwnrTm = DEF_SPAWNTM;
+        spwnrTm = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        spwnrTm -= Time.deltaTime;
+        if(spwnrTm <= 0)
+        {
+            spwnrTm = maxSpwnrTm;
+            spawnSpawner();
+        }
+	}//end Update
+
+    //Chooses a random object (in ObjsType) and creates a spawner of it
+    private void spawnSpawner()
+    {
+        int totalWeight = 0;
+        foreach (ObjType element in System.Enum.GetValues(typeof(ObjType)))
+        {
+            totalWeight += Objs.getWeight(element);
+        }
+
+        int select = Random.Range(0, totalWeight);
+
+        foreach (ObjType element in System.Enum.GetValues(typeof(ObjType)))
+        {
+            if (select <= Objs.getWeight(element))
+            {
+                Objs.changeWeights(element);
+                Debug.Log("<color=green>We are adding " + element.ToString()
+                    + " spawner</color>");
+                GameObject spwnInst = Instantiate(spawner) as GameObject;
+                spwnInst.GetComponent<Spawner>().createSpawner(element.ToString(), diff);
+                allSpawners.Add(spwnInst);
+                break;
+            }
+        }
+
+    }//end spawnSpawner
+}
---
+++ New Unity Project 1/Assets/Scripts/Obs/Spawner.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+    private const float DEF_SPWNTM = 6; // because 8 is a good number
+    private float spawnTime, diff, maxSpawnTime;
+	public float xSpawn;
+	public int totalLanes;
+	public List <GameObject> allCurrentObs;
+	private GameObject obs;
+
+
+	// Use this for initialization
+	void Start () {
+        allCurrentObs = new List<GameObject>();
+	}//end Start
+	
+	// Update is called once per frame
+	void Update () {
+		if (spawnTime <= 0)
+			randomSpawn ();
+		spawnTime -= Time.deltaTime;
+	}//end Update
+
+    //Allows us to create new spawnners with w/e starting conditions we want. xStart and lanes
+    //might be worked out in future releases
+     public void createSpawner(string obsType = "Cube", float strtDif = 1, 
+        int lanes = 1, float xStart = 15)
+    {
+        obs = Objs.loadType(Objs.toObjType(obsType));
+
+        diff = strtDif;
+        xSpawn = xStart;
+        totalLanes = lanes;
+        maxSpawnTime = DEF_SPWNTM / diff;
+    }//end createSpawnner
+
+    //Allows us to incremente diff over time, or to change diff
+    //if user finds game too hard
+    public void chngDiff(float diffChng, bool setValue = false)
+    {
+        if (setValue)
+            diff = diffChng;
+        else
+            diff = diff + diffChng;
+        maxSpawnTime = DEF_SPWNTM / diff;
+    }//end chngDiff
+
+    public void Remove(GameObject obs)
+    {
+        allCurrentObs.Remove(obs);
+        spawnTime = 0.0f;
+    }//end Remove
+
+    public void HideObs()
+    {
+        int temp = allCurrentObs.Count;
+        for (int i = 0; i < temp; i++)
+        {
+            Renderer reference = allCurrentObs[i].GetComponent<Renderer>();
+            reference.enabled = false;
+        }
+    }//end HideObs
+
+    public void UnHideObs()
+    {
+        int temp = allCurrentObs.Count;
+        for (int i = 0; i < temp; i++)
+        {
+            Renderer reference = allCurrentObs[i].GetComponent<Renderer>();
+            reference.enabled = true;
+        }
+    }//end UnHideObs
+
+    private void randomSpawn()
+	{
+		GameObject thisInstince = Instantiate (obs, new Vector3 (xSpawn, totalLanes, 0),
+			Quaternion.identity) as GameObject;
+		thisInstince.GetComponent<Obstacles> ().
+            setValues(totalLanes, 1, 5, this.gameObject, true);
+        allCurrentObs.Add(thisInstince);
+		spawnTime = maxSpawnTime;
+	}//end randomSpawn
+}
