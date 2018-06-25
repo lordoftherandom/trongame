f34479a1ebe40c368a3bf686a97589fa2e0a3a00
lordoftherandom <dietokillfun@gmail.com>
2018-06-25 08:00:29 -0400

    Controller Re-work
    Reworked the controller input to get rid of jitter into walls

--- New Unity Project 1/Assets/Scripts/Controller.cs
+++ New Unity Project 1/Assets/Scripts/Controller.cs
@@ -50,11 +50,36 @@ public class Controller : MonoBehaviour {
         float xDir = x * speed, yDir = y * speed;
 
         Vector2 velocity = Vector2.zero;
-        Vector2 strtPos = hero.transform.localPosition;
+        Vector2 strtPos = transform.position;
         Vector2 trgPos = new Vector2(xDir , yDir) + strtPos;
-        hero.transform.localPosition = Vector2.SmoothDamp(strtPos, trgPos, ref velocity, smoothTime);
+        RaycastHit2D[] hits = new RaycastHit2D[2];
+
+        hits[0] = castARay(new Vector2(xDir, 0) + strtPos);
+        hits[1] = castARay(new Vector2(0, yDir) + strtPos);
+
+        if (hits[0].collider != null)
+            trgPos.x = strtPos.x;
+        if (hits[1].collider != null)
+            trgPos.y = strtPos.y;
+
+        transform.position = Vector2.SmoothDamp(strtPos, trgPos, ref velocity, smoothTime);
     }
 
     void roughMove(float x, float y) { } //blank class, to be filled later
+
+    RaycastHit2D castARay(Vector2 target)
+    {
+        Vector2 dir = target - (Vector2)this.transform.position;
+        dir.Normalize();
+
+        float length;
+        if (Mathf.Abs(dir.x) > 0)
+            length = 0.65f;
+        else
+            length = 1.15f;
+        Debug.DrawRay((Vector2)transform.position, dir*length, Color.red, 1, false);
+        return Physics2D.Raycast((Vector2)transform.position, dir, length);
+    }
+    
     #endregion
 }
